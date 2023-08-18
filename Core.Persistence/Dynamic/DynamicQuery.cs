e833e01e1eb4be883500883089bd78c1ca19efd5
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2023-08-18 16:48:59 +0300

    Dynamic Query implementation

---
+++ Core.Persistence/Dynamic/DynamicQuery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Dynamic;
+
+public class DynamicQuery
+{
+    public IEnumerable<Sort>? Sort { get; set; } // sort'u list geçerken
+    public Filter? Filter { get; set; } // filter'ı tek geçiyoruz
+
+    public DynamicQuery()
+    {
+
+    }
+
+    public DynamicQuery(IEnumerable<Sort>? sort, Filter? filter)
+    {
+        Filter = filter;
+        Sort = sort;
+    }
+}
